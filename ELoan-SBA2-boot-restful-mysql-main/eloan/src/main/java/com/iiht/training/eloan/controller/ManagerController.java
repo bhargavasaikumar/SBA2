146e4609d265910595e4cec807a88c81f344a91f
bhargavasaikumar <73290583+bhargavasaikumar@users.noreply.github.com>
2020-12-05 18:04:39 +0530

    Add files via upload

--- ELoan-SBA2-boot-restful-mysql-main/eloan/src/main/java/com/iiht/training/eloan/controller/ManagerController.java
+++ ELoan-SBA2-boot-restful-mysql-main/eloan/src/main/java/com/iiht/training/eloan/controller/ManagerController.java
@@ -2,9 +2,12 @@ package com.iiht.training.eloan.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +22,7 @@ import com.iiht.training.eloan.dto.SanctionDto;
 import com.iiht.training.eloan.dto.SanctionOutputDto;
 import com.iiht.training.eloan.dto.exception.ExceptionResponse;
 import com.iiht.training.eloan.exception.AlreadyFinalizedException;
+import com.iiht.training.eloan.exception.InvalidDataException;
 import com.iiht.training.eloan.exception.ManagerNotFoundException;
 import com.iiht.training.eloan.service.ManagerService;
 
@@ -28,45 +32,46 @@ public class ManagerController {
 
 	@Autowired
 	private ManagerService managerService;
-	
+
 	@GetMapping("/all-processed")
 	public ResponseEntity<List<LoanOutputDto>> allProcessedLoans() {
-		return null;
+		return new ResponseEntity<List<LoanOutputDto>>(managerService.allProcessedLoans(), HttpStatus.OK);
 	}
-	
+
 	@PostMapping("/reject-loan/{managerId}/{loanAppId}")
-	public ResponseEntity<RejectDto> rejectLoan(@PathVariable Long managerId,
-												@PathVariable Long loanAppId,
-												@RequestBody RejectDto rejectDto){
-		return null;
+	public ResponseEntity<RejectDto> rejectLoan(@PathVariable Long managerId, @PathVariable Long loanAppId,
+			@Valid @RequestBody RejectDto rejectDto, BindingResult bindingResult) throws InvalidDataException {
+		if (bindingResult.hasErrors()) {
+			throw new InvalidDataException("Remarks are incorrect");
+		}
+		return new ResponseEntity<RejectDto>(managerService.rejectLoan(managerId, loanAppId, rejectDto), HttpStatus.OK);
 	}
-	
+
 	@PostMapping("/sanction-loan/{managerId}/{loanAppId}")
-	public ResponseEntity<SanctionOutputDto> sanctionLoan(@PathVariable Long managerId,
-												@PathVariable Long loanAppId,
-												@RequestBody SanctionDto sanctionDto){
-		return null;
+	public ResponseEntity<SanctionOutputDto> sanctionLoan(@PathVariable Long managerId, @PathVariable Long loanAppId,
+			@Valid @RequestBody SanctionDto sanctionDto, BindingResult bindingResult) throws InvalidDataException {
+		if (bindingResult.hasErrors()) {
+			throw new InvalidDataException("Invalid details provided to sanction the loan");
+		}
+		return new ResponseEntity<SanctionOutputDto>(managerService.sanctionLoan(managerId, loanAppId, sanctionDto),
+				HttpStatus.OK);
 	}
-	
+
 	@ExceptionHandler(ManagerNotFoundException.class)
-	public ResponseEntity<ExceptionResponse> handler(ManagerNotFoundException ex){
-		ExceptionResponse exception = 
-				new ExceptionResponse(ex.getMessage(),
-									  System.currentTimeMillis(),
-									  HttpStatus.NOT_FOUND.value());
-		ResponseEntity<ExceptionResponse> response =
-				new ResponseEntity<ExceptionResponse>(exception, HttpStatus.NOT_FOUND);
+	public ResponseEntity<ExceptionResponse> handler(ManagerNotFoundException ex) {
+		ExceptionResponse exception = new ExceptionResponse(ex.getMessage(), System.currentTimeMillis(),
+				HttpStatus.NOT_FOUND.value());
+		ResponseEntity<ExceptionResponse> response = new ResponseEntity<ExceptionResponse>(exception,
+				HttpStatus.NOT_FOUND);
 		return response;
 	}
-	
+
 	@ExceptionHandler(AlreadyFinalizedException.class)
-	public ResponseEntity<ExceptionResponse> handler(AlreadyFinalizedException ex){
-		ExceptionResponse exception = 
-				new ExceptionResponse(ex.getMessage(),
-									  System.currentTimeMillis(),
-									  HttpStatus.BAD_REQUEST.value());
-		ResponseEntity<ExceptionResponse> response =
-				new ResponseEntity<ExceptionResponse>(exception, HttpStatus.BAD_REQUEST);
+	public ResponseEntity<ExceptionResponse> handler(AlreadyFinalizedException ex) {
+		ExceptionResponse exception = new ExceptionResponse(ex.getMessage(), System.currentTimeMillis(),
+				HttpStatus.BAD_REQUEST.value());
+		ResponseEntity<ExceptionResponse> response = new ResponseEntity<ExceptionResponse>(exception,
+				HttpStatus.BAD_REQUEST);
 		return response;
 	}
 }
--- ELoan-SBA2-boot-restful-mysql-main/eloan/src/main/java/com/iiht/training/eloan/dto/RejectDto.java
+++ ELoan-SBA2-boot-restful-mysql-main/eloan/src/main/java/com/iiht/training/eloan/dto/RejectDto.java
@@ -3,6 +3,16 @@ package com.iiht.training.eloan.dto;
 public class RejectDto {
 	private String remark;
 
+	public RejectDto(String remark) {
+		super();
+		this.remark = remark;
+	}
+
+	public RejectDto() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
 	public String getRemark() {
 		return remark;
 	}
@@ -10,6 +20,5 @@ public class RejectDto {
 	public void setRemark(String remark) {
 		this.remark = remark;
 	}
-	
-	
+
 }
